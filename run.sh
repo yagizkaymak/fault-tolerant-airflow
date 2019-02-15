b980789cafd04d8749f6c8277ab498bb0c9124d2
yagizkaymak <yagiz.kaymak@gmail.com>
2019-02-15 17:23:16 -0500

    Secret is copied locally.

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 yes | airflow resetdb
 airflow initdb
 yes | cp -rf src/dags/PDS_DAG.py /home/ubuntu/airflow/dags
+yes | cp -rf /home/ubuntu/Projects/PDS/src/main/python/secret.py /home/ubuntu/fault-tolerant-airflow/src/dags
+yes | cp -rf /home/ubuntu/Projects/PDS/src/main/python/secret.py /home/ubuntu/fault-tolerant-airflow/src/spark
+yes | cp -rf /home/ubuntu/Projects/PDS/src/main/python/secret.py /home/ubuntu/fault-tolerant-airflow/src/ft_airflow
